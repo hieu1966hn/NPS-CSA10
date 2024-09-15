e0dbd6d10211189b5c34adfe21f3833621437b54
hieu1966hn <hieu1966hn@gmail.com>
2024-09-15 09:01:17 +0700

    New code Animal

--- SS2/SS2.py
+++ SS2/SS2.py
@@ -31,4 +31,28 @@
 # ### Trừ 2 điểm trên
 # print("Trừ 2 điểm point1 và point2", point1.sub(point2))
 
-# ### 
+
+
+#### Bài thực hành với kế thừa:
+class Animal:
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+    
+    def make_sound(self):
+        return f'{self.name} makes a sound'
+        
+class Sheep(Animal):
+    def __init__(self, name, species, breed):
+        ## Gọi làm hàm khởi tạo của lớp cha và kế thừa chúng: super()
+        super().__init__(name, species)
+        self.breed = breed
+
+    ### Mặc định có phương thức make_sound(). tuy nhiên muốn bổ sung thêm 
+    def make_sound(self):
+        ## Sử dụng phương thức kế thừa super().phương_thức muốn kế thừa
+        return super().make_sound() + f". {self.name} Beeee"
+
+### Tạo một đối tượng cừu: 
+sheep1 = Sheep("Blue", "Sheep", "Cừu Beltex")
+print(sheep1.make_sound())
--- SS2/SS2_lythuyet.md
+++ SS2/SS2_lythuyet.md
@@ -1,1 +1,3 @@
 Buổi 2: Kế thừa trong lập trình hướng đối tượng (OOP)
+
+- Kế thừa (inheritance): Là một trong 4 nguyên lý cơ bản của lập trình hướng đối tượng. Kế thừa cho phép chúng ta tạo ra một lớp (gọi là subclass) dựa trên một lớp hiện có (gọi là lớp cha hoặc superclass). Lớp con thừa hưởng tất cả các thuộc tính và phương thức của lớp cha nhưng cũng có thể thêm hoặc ghi đè các thuộc tính và phương thức của riêng lớp con.
